19f1a0474879912c145e0acfbd3735ab441ccc93
eloisa200 <elotavares.araujo@gmail.com>
2024-11-05 17:21:55 -0300

    Add files via upload

---
+++ finalboss/script.js
@@ -0,0 +1,6 @@
+// Função para adicionar ingresso ao carrinho
+function adicionarAoCarrinho() {
+    alert("Ingresso adicionado ao carrinho!");
+}
+
+// Aqui você pode adicionar outras funcionalidades no futuro, como persistir os dados do carrinho ou fazer interações com APIs.
